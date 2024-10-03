7e87aee648823d83fa724b98343acaf94a745894
itsdylantan <dylantan16@gmail.com>
2024-10-02 22:19:15 -0700

    Updatedf styling for contact me page

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -6,9 +6,11 @@ const Contact = () => {
   return (
     <section className="contact">
       <h2>Contact Me</h2>
-      <p>Phone: +1 (250) 864-1680</p>
-      <p>Email: dylantan16@gmail.com</p>
-      <p>GitHub: <a href="https://github.com/itsdylantan">https://github.com/itsdylantan</a></p>
+      <div className="contact-details">
+        <p><span>Phone:</span> +1 (250) 864-1680</p>
+        <p><span>Email:</span> <a href="mailto:dylantan16@gmail.com">dylantan16@gmail.com</a></p>
+        <p><span>GitHub:</span> <a href="https://github.com/itsdylantan" target="_blank" rel="noopener noreferrer">https://github.com/itsdylantan</a></p>
+      </div>
     </section>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,7 +13,7 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <Link to="/" className="navbar-logo"> {/* Link added to the logo */}
-        <h1>Dylan Tan</h1>
+        <h1>Dylan's Portfolio</h1>
       </Link>
       
       <div className={`hamburger ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
@@ -24,11 +24,11 @@ const Navbar = () => {
       
       <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
         <li><Link to="/" onClick={toggleMenu}>Home</Link></li>
-        <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
+        <li><Link to="/projects" onClick={toggleMenu}>Projects</Link></li>
         <li><Link to="/resume" onClick={toggleMenu}>Resume</Link></li>
         <li><Link to="/work-experience" onClick={toggleMenu}>Work Experience</Link></li>
-        <li><Link to="/projects" onClick={toggleMenu}>Projects</Link></li>
-        <li><Link to="/contact" onClick={toggleMenu}>Contact</Link></li>
+        <li><Link to="/about" onClick={toggleMenu}>About</Link></li>
+        <li><Link to="/contact" onClick={toggleMenu}>Contact Me</Link></li>
       </ul>
     </nav>
   );
